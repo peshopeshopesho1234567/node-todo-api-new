3c7e4e964b1db56d66edb11b15d8fadc509eaa49
Grigor P. Penev <Grigor.Penev@verint.com>
2018-06-04 18:00:05 +0300

    Covered the case where id is valid but todo does not exist.

--- server/server.js
+++ server/server.js
@@ -34,10 +34,15 @@ app.get("/todos/:id", (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        return res.status(404).send({error: "Id is not valid"});
+        return res.status(404).send({error: "Id is not valid."});
     }
 
     Todo.findById(id).then( todo => {
+
+        if (!todo) {
+            return res.status(404).send({error: "Todo not found."});
+        }
+
         res.send(todo);
     }, e => {
         res.status(400).send(e);
