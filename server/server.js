f8ccfa38e263820c690dd504ee1e496ce91784b0
Grigor P. Penev <Grigor.Penev@verint.com>
2018-06-05 09:09:25 +0300

    Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.post("/todos", (req, res) => {
     todo.save().then( doc => {
         res.send(doc);
     }, e => {
-        res.status(400).send(e);
+        res.status(400).send();
     });
 });
 
@@ -26,7 +26,7 @@ app.get("/todos", (req, res) => {
     Todo.find().then( todos => {
         res.send({todos});
     }, e => {
-        res.status(404).send(e);
+        res.status(404).send();
     });
 });
 
@@ -45,7 +45,26 @@ app.get("/todos/:id", (req, res) => {
 
         res.send({todo});
     }, e => {
-        res.status(404).send(e);
+        res.status(404).send();
+    });
+});
+
+app.delete("/todos/:id", (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({error: "Id is not valid"});
+    }
+
+    Todo.findByIdAndRemove(id).then( todo => {
+
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }, e => {
+        res.status(404).send();
     });
 });
 
